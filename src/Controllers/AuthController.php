c9c1a9722de6bf9a629dbe4ddf9f2dade2c6fcce
xhris112003 <117724527+xhris112003@users.noreply.github.com>
2023-01-09 15:56:49 +0100

    v1.1.5

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -4,40 +4,72 @@
   use App\Controller;
   use App\Request;
   use App\Session;
+  use App\Container;
+  use App\Models\Model;
+  use App\Models\Usuari; 
+
 
   final class AuthController extends Controller{
     // $this->qb es el constructor de consultes
-   function __construct(Request $request,Session $session){
+    function __construct(Request $request,Session $session){
       parent::__construct($request,$session);
     }
 
+    public function index()
+    {
+      //mostrar form
+      return view('auth');
+     
+    }
     function signin(){
       //capturar elements de POST
-      $username=$this->request->post('username');   
+      $email=$this->request->post('email');   
       $passwd=$this->request->post('password');
       //crida al metode privat d'autenticació
-      $this->auth($username,$passwd);  
+      $this->auth($email,$passwd);  
     }
     
-    private function auth(string $userame,string $passwd){
-      $password=password_hash($passwd,PASSWORD_BCRYPT,['cost'=>4]);
-       $result= $this->qb->select(['username','password'])->from('usuario')
-        ->where(['username'=>$username])->limit(1)->exec()->fetch(); 
-        if($result){
-          $this->redirect('/dashboard');
-        }else{
+    private function auth(string $email,string $passwd){
+      //$password=password_hash($passwd,PASSWORD_BCRYPT,['cost'=>4]);
+       $res=$this->qb->select(['*'])->from('usuaris')
+        ->where(['email'=>$email])->limit(1)->exec()->fetch();
+    
+      if($res){
+        $user=$res[0];
+      
+          //die($user->passwd);
+          if (password_verify($passwd,$user->passwd)){
+             Session::set('user',$user);
+            //desar servei auth
+            $this->redirect('/dashboard');
+            }         
+          }
+      else{
           $this->session->set('error',"Sessión fallida");
           $this->redirect('/auth');
         }
       
      
     }
-    public function prueba()
-    {
-      echo "Prueba - HomeController";
-      //render vista home
+    
+    function signup(){
+      $email=$this->request->post('email');   
+      $passwd=$this->request->post('passwd');
+      $password=password_hash($passwd,PASSWORD_BCRYPT,['cost'=>4]);
+      $roles_id='2';
+      $data=['email'=>$email,
+        'passwd'=>$password,
+        'roles_id'=>$roles_id];
+        $user=new Usuari($data);
+      //persist on DB
+        if($user->persist()){
+          $this->redirect('/');
+        };
     }
-    public function error(){
-      echo "error";
+    
+    function logout(){
+      $this->session->destroy();
+      $this->redirect('/');
     }
+    
   }
--- src/Controllers/DashboardController.php
+++ src/Controllers/DashboardController.php
@@ -3,24 +3,49 @@
   namespace App\Controllers;
 
 use App\Controller;
+use App\Container;
 use App\Request;
 use App\Session;
+use App\Models\Llibre;
+use App\Models\Prestec;
+use App\Models\Usuari;
+
   final class DashboardController extends Controller{
-    
+    protected Usuari $user;
     function __construct(Request $request,Session $session){
       parent::__construct($request,$session);
+      $user=new Usuari(Session::get('usuario'));
+      var_dump($user);
+      die;
+      $this->user=$user;
     }
 
     public function index()
     {
+      $user=Session::get('usuario');
       //primer obtenir dades
-     $cataleg=$this->qb->select(['*'])->from('libro')->exec();
-      return view('dashboard');
-      //render vista home
+      $llibres=new Llibre();
+      $cataleg=$llibres->find(['disponible'=>true]);    
+      return view('dashboard', ['cataleg'=>$cataleg,'usuario'=>$user]); 
     }
-   
+   function reserva(){
+     $id=$this->request->getParams();
+     $llibre=(new Llibre())->find(['id'=>$id])[0];
+     return view('reserva',['llibre'=>$llibre,
+                 'user'=>$this->user]);
+   }
     
-    public function error(){
-      echo "error";
+  function prestec(){
+      //crear prèstec
+       $id=$this->request->getParams();
+       $llibre=(new Llibre())->find(['id'=>$id])[0];
+       $prestec=new Prestec($this->user,$llibre);
+       var_dump($prestec);
+      die;
+        
+    }
+    
+    function prestecs(){
+      echo "prestecs";
     }
   }
